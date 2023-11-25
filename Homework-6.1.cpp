b6fc2d706de21a2aacf51b4260e13b1bb545270a
SkeTch <sketch13rus@mail.ru>
2023-11-25 15:22:58 +0300

    Initial commit

---
+++ Homework-6.1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+
+int main() {
+    std::string text;
+    std::cout << "[IN]: ";
+    std::getline(std::cin, text);
+
+    std::map<char, int> symbolFrequency;
+
+    for (char c : text) {
+        symbolFrequency[c]++;
+    }
+
+    std::map<int, std::vector<char>, std::greater<>> sortedFrequency;
+    for (const auto& pair : symbolFrequency) {
+        sortedFrequency[pair.second].push_back(pair.first);
+    }
+
+    std::cout << "[OUT]:\n";
+    for (const auto& pair : sortedFrequency) {
+        for (char c : pair.second) {
+            std::cout << c << ": " << pair.first << std::endl;
+        }
+    }
+
+    return 0;
+}
